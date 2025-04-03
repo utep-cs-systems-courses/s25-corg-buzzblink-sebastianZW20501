c046bcdb91de10d573b7275b26cf5dd288c4d7be
sebastianZW20501 <sebastianzepedawaissman@gmail.com>
2025-04-03 10:50:04 -0600

    Update buzzer.c

--- project/buzzer.c
+++ project/buzzer.c
@@ -4,13 +4,7 @@
 
 void buzzer_init()
 {
-    /* 
-       Direct timer A output "TA0.1" to P2.6.  
-        According to table 21 from data sheet:
-          P2SEL2.6, P2SEL2.7, anmd P2SEL.7 must be zero
-          P2SEL.6 must be 1
-        Also: P2.6 direction must be output
-    */
+
     timerAUpmode();		/* used to drive speaker */
     P2SEL2 &= ~(BIT6 | BIT7);
     P2SEL &= ~BIT7; 
